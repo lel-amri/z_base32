411d95e7319edb0a28dc86e247e1df3625d43553
Léo El Amri <leo@superlel.me>
2018-09-09 21:05:40 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+
+setuptools.setup(
+    name="z_base32",
+    version="0.1",
+    author="Léo El Amri",
+    author_email="leo@superlel.me",
+    description="A small package implementing z-base-32 encoding/decoding",
+    url="https://github.com/lel-amri/z_base32",
+    py_modules=['z_base32'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
